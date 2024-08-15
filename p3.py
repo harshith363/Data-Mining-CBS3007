e5bcbd7bc88016f7c2b6edf327f1fccc8d08f37a
harshit363 <harshithvasanthkumar@gmail.com>
2024-08-15 23:34:15 +0530

    khsbvkf

--- p3.py
+++ p3.py
@@ -2,36 +2,28 @@ import pandas as pd
 from mlxtend.frequent_patterns import fpgrowth, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 
-# Load the dataset
-file_name = r'C:\Users\harsh\OneDrive\Documents\VIT\FALLSEM 24-25\CBS3007\DA1 main\transactions.csv'
+file_name = r'C:\Users\harsh\OneDrive\Documents\VIT\FALLSEM 24-25\CBS3007\DA1 main\cars.csv'
 df = pd.read_csv(file_name)
 
-# Convert the DataFrame to a list of transactions
-transactions = df.groupby('TransactionID')['Item'].apply(list).tolist()
+car = df.groupby('TransactionID')['Item'].apply(list).tolist()
 
-# Convert transactions to one-hot encoded DataFrame
 te = TransactionEncoder()
-te_ary = te.fit(transactions).transform(transactions)
+te_ary = te.fit(car).transform(car)
 df_onehot = pd.DataFrame(te_ary, columns=te.columns_)
 
-# Define the minimum support threshold
-min_support = 0.4  # 40%
+min_support = 0.4
 
-# Find frequent itemsets using FP-Growth
 frequent_itemsets = fpgrowth(
     df_onehot, min_support=min_support, use_colnames=True)
 
-# Print frequent itemsets
+print("Harshith 21BBS0163")
 print("Frequent Itemsets:")
 print(frequent_itemsets)
 
-# Define the minimum confidence threshold
-min_confidence = 0.7  # 70%
+min_confidence = 0.7
 
-# Generate association rules
 rules = association_rules(
     frequent_itemsets, metric="confidence", min_threshold=min_confidence)
 
-# Print association rules
 print("\nAssociation Rules:")
 print(rules)
